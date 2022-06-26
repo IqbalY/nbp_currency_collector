21e264c3cf108855deffd28f40a2a655cee4e02f
Iqbal Y <iqbal.y@promarlins.com>
2022-06-27 00:32:05 +0500

    fix(schema): Column name modification

--- db/migrate/20220626072314_create_exchange_tables.rb
+++ db/migrate/20220626072314_create_exchange_tables.rb
@@ -2,8 +2,7 @@ class CreateExchangeTables < ActiveRecord::Migration[7.0]
   def change
     create_table :exchange_tables do |t|
       t.integer :tab_type
-      t.string :tab_num
-      t.datetime :published_at
+      t.string :published_at
 
       t.timestamps
     end
--- db/migrate/20220626073228_create_rates.rb
+++ db/migrate/20220626073228_create_rates.rb
@@ -1,9 +1,9 @@
 class CreateRates < ActiveRecord::Migration[7.0]
   def change
     create_table :rates do |t|
-      t.string :currency_name
-      t.string :currency_code
-      t.decimal :average_value
+      t.string :currency
+      t.string :code
+      t.decimal :mid
       t.references :exchange_table, null: false, foreign_key: true
 
       t.timestamps
